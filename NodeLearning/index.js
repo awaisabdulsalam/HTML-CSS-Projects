0867ddb0bf7fb93d5adf65fe2188ff199191e4ca
Awais Abdul Salam <theawais170@gmail.com>
2023-08-03 23:25:50 +0500

    Express learning

--- NodeLearning/index.js
+++ NodeLearning/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 //?   Humari saari App iss " app " variable ma hy
 const app = express();
+const form = require("./routes/form")
 
 //*   Main work of Middleware is to act as MIDDLE WARE
 //?     Middleware Function
@@ -14,9 +15,11 @@ app.use((req, res, next) => {
 })
 
 //! 2. Department
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
     // res.send(req.url);
-    res.send(req.data);
-})
+    // res.send(req.data);
+// })
+
+app.use("/form", form)
 
 app.listen(3000);
--- NodeLearning/routes/form.js
+++ NodeLearning/routes/form.js
@@ -1,1 +1,13 @@
-const exp
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send(`
+    <form>
+    <input name='data' />
+    <button>Submit</button>
+    </form>
+    `)
+})
+
+module.exports = router;
