07c42b2049a9a1e26e10cba8e63f05b5b80af7c4
Awais Abdul Salam <theawais170@gmail.com>
2023-08-04 23:13:05 +0500

    prac

--- NodeLearning/index.js
+++ NodeLearning/index.js
@@ -1,25 +1,47 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const form = require("./routes/form");
+const path = require("path");
+
 //?   Humari saari App iss " app " variable ma hy
 const app = express();
-const form = require("./routes/form")
+
 
 //*   Main work of Middleware is to act as MIDDLE WARE
+//?   Middleware is like different Departments in Company
+//?   These all functions are called MIDDLEWARE functions
+
+//! 1. Depratment
+//*    Received Data ko Manage krne k liye " bodyParser " use hoga
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+//! 2. Department
+//?     Ye iss liye k koi bhi iss folder ko access kr skta hy ( like: Client Side )
+app.use(express.static(path.join(process.cwd(), "public")))
+
+//?     Konsa Engine
+app.use("view engine", "ejs");
+//?     Konsa folder (Views k liye)
+app.use("views", "views");
+
+//! 3. Department
 //?     Middleware Function
-//?     Middleware is like different Departments in Company
-//! 1. Department
-app.use((req, res, next) => {
+app.use((req, res, next) => {        //?  Auth Middleware
     // console.log(req.url);
     req.data = "Awais";
     next();
     //?     "next()" agly department(function) ko call kr de ga 
 })
 
-//! 2. Department
+//! 3. Department
 // app.use((req, res, next) => {
     // res.send(req.url);
     // res.send(req.data);
 // })
 
-app.use("/form", form)
-
+//! 4. Department
+app.use("/form", form);
 app.listen(3000);
--- NodeLearning/routes/form.js
+++ NodeLearning/routes/form.js
@@ -1,13 +1,30 @@
 const express = require("express");
+const path = require("path");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send(`
-    <form>
-    <input name='data' />
-    <button>Submit</button>
-    </form>
-    `)
-})
+  //! Before
+  // res.send(`
+  // <form action="form/submit" method="POST">
+  // <input name='data' />
+  // <button>Submit</button>
+  // </form>
+  // `)
+  //!  After
+  //?  sendFile because we sending whole FILE
+  // res.sendFile(path.join(process.cwd(), "views", "form.html"));
+  //!  After 2
+  //*  Now we don't need to write file path like ABOVE
+  //*  We already have defined in index.js file
+  res.render("form");
+});
+
+router.post("/submit", (req, res) => {
+  //*  bodyParser se ho k Data ithar " req.body " ma mil jaye ga
+
+  // res.send("Submitted");
+  res.send(req.body.data);
+});
 
 module.exports = router;
